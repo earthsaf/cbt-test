8725520b20ed264717712c2be07d2a53113b184a
earthsaf <bryanokechukwu596@gmail.com>
2025-06-20 00:43:38 +0100

    versionn

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const { requireAuth } = require('../middlewares/auth');
 const router = express.Router();
 
 router.post('/login', authController.login);
