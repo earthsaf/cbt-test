087b976bfc64bab378fb61b890bdcd5e639ab18c
earthsaf <bryanokechukwu596@gmail.com>
2025-08-06 23:04:02 +0100

    'hhh

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
